d816dddbb8bac2a440d1e87db23de5208708fa85
ronne-k <166863210+ronne-k@users.noreply.github.com>
2024-04-14 16:56:33 -0700

    Update project_regression_code.py

--- project_regression_code.py
+++ project_regression_code.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
 from pmdarima.arima import auto_arima
-from dataframe import DataAnalysis as df
+from dataframe import DataAnalysis
 
+# Step 1: Need to create instance of DataAnalysis
+df_instance = DataAnalysis(file_name)
+df_instance.analyze_data()
 # Step 2: Prepare data
-df['date'] = pd.to_datetime(df.incident_dates)
+df_instance['date'] = pd.to_datetime(df_instance.incident_dates)
 
 # Step 3: Fit the regression model
-x = df.x_values
-y = df.y_values
+x = df_instance.x_values
+y = df_instance.y_values
 
-model = auto_arima(df.y_values, exogenous = df.x_values, seasonal=False) # Ordinary Least Squares regression
+model = auto_arima(y, exogenous=x, seasonal=False) # Ordinary Least Squares regression
 results = model.fit()
 
 # Step 4: Evaluate the model
